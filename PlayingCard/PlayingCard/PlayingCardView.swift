1e28904cdcb04c244370d9ba6c632ecffba4d0d1
Soojung-Shin <ssj15987@naver.com>
2019-12-22 17:47:14 +0900

    카드 뷰 추가

---
+++ PlayingCard/PlayingCard/PlayingCardView.swift
@@ -0,0 +1,15 @@
+//
+//  PlayingCardView.swift
+//  PlayingCard
+//
+//  Created by Soojung Shin on 2019/12/22.
+//  Copyright © 2019 Soojung Shin. All rights reserved.
+//
+
+import UIKit
+
+class PlayingCardView: UIView {
+
+    override func draw(_ rect: CGRect) {
+    }
+}
