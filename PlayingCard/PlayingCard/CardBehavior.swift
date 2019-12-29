b5f708a1ba387a94f18ce8da799d077d765122df
Soojung-Shin <ssj15987@naver.com>
2019-12-29 18:01:11 +0900

    카드가 모서리에 모이지 않게 수정

--- PlayingCard/PlayingCard/CardBehavior.swift
+++ PlayingCard/PlayingCard/CardBehavior.swift
@@ -25,7 +25,22 @@ class CardBehavior: UIDynamicBehavior {
 
     func push(_ item: UIDynamicItem) {
         let pushBehavior = UIPushBehavior(items: [item], mode: .instantaneous)
-        pushBehavior.angle = CGFloat(Int(2 * CGFloat.pi).arc4random) + CGFloat(drand48())
+        //카드 뷰가 화면의 모서리에 모여있지 않도록 push 애니메이션의 angle을 중앙을 향하도록 한다.
+        if let referenceBounds = dynamicAnimator?.referenceView?.bounds {
+            let center = CGPoint(x: referenceBounds.midX, y: referenceBounds.midY)
+            switch (item.center.x, item.center.y) {
+            case let (x, y) where x < center.x && y < center.y:
+                pushBehavior.angle = (CGFloat.pi / 2).arc4random
+            case let (x, y) where x > center.x && y < center.y:
+                pushBehavior.angle = CGFloat.pi - (CGFloat.pi / 2).arc4random
+            case let (x, y) where x < center.x && y > center.y:
+                pushBehavior.angle = (-CGFloat.pi / 2).arc4random
+            case let (x, y) where x < center.x && y < center.y:
+                pushBehavior.angle = CGFloat.pi + (CGFloat.pi / 2).arc4random
+            default:
+                pushBehavior.angle = (CGFloat.pi * 2).arc4random
+            }
+        }
         pushBehavior.magnitude = 1 + CGFloat(drand48())
         //pushBehavior는 일회성 애니메이션이기 때문에 사용되면 삭제해주는 것이 좋다. 액션에서 처리해주는데, 이때 애니메이터와 behavior 클로져가 서로를 가리키고 있기 때문에 메모리 사이클이 발생한다. unowned와 weak를 사용해 클로져에 변수를 선언하여 힙에 존재하지않도록 설정하여 해결할 수 있다.
         pushBehavior.action = { [unowned pushBehavior, weak self] in
@@ -55,5 +70,17 @@ class CardBehavior: UIDynamicBehavior {
     convenience init(in animator: UIDynamicAnimator) {
         self.init()
         animator.addBehavior(self)
+    }
+}
+
+extension CGFloat {
+    var arc4random: CGFloat {
+        if self > 0 {
+            return CGFloat(arc4random_uniform(UInt32(self - 1))) + CGFloat(drand48())
+        } else if self < 0 {
+            return -(CGFloat(arc4random_uniform(UInt32(abs(self - 1)))) + CGFloat(drand48()))
+        } else {
+            return 0
+        }
     }
 }
