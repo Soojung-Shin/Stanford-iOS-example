b38620a1bc63655e53b823a84ee12bce58eba846
Soojung-Shin <ssj15987@naver.com>
2020-01-02 16:49:10 +0900

    이모티콘을 선택하는 컬렉션 뷰 추가

--- EmojiArt/EmojiArt/EmojiArtViewController.swift
+++ EmojiArt/EmojiArt/EmojiArtViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class EmojiArtViewController: UIViewController, UIDropInteractionDelegate, UIScrollViewDelegate {
+class EmojiArtViewController: UIViewController, UIDropInteractionDelegate, UIScrollViewDelegate, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     
     //drop zone 뷰를 연결하고, drop 인터렉션을 추가한다.
     @IBOutlet weak var dropZone: UIView! {
@@ -58,6 +58,32 @@ class EmojiArtViewController: UIViewController, UIDropInteractionDelegate, UIScr
                 scrollView.zoomScale = max(dropZone.bounds.size.width / size.width, dropZone.bounds.size.height / size.height)
             }
         }
+    }
+    
+    var emojis = "😀😇😌😍🥰😘😎🤓😈🌛🌞⭐️🌎🍎🍉🍒🥑⚽️".map { String($0) }
+    
+    @IBOutlet weak var emojiCollectionView: UICollectionView! {
+        didSet {
+            emojiCollectionView.dataSource = self
+            emojiCollectionView.delegate = self
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return emojis.count
+    }
+    
+    private var font: UIFont {
+        return UIFontMetrics(forTextStyle: .body).scaledFont(for: UIFont.preferredFont(forTextStyle: .body)).withSize(64.0)
+    }
+        
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "EmojiCell", for: indexPath)
+        if let emojiCell = cell as? EmojiCollectionViewCell {
+            let text = NSAttributedString(string: emojis[indexPath.item], attributes: [.font: font])
+            emojiCell.label.attributedText = text
+        }
+        return cell
     }
     
     
---
+++ EmojiArt/EmojiArt/EmojiCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  EmojiCollectionViewCell.swift
+//  EmojiArt
+//
+//  Created by Soojung Shin on 2020/01/02.
+//  Copyright © 2020 Soojung Shin. All rights reserved.
+//
+
+import UIKit
+
+class EmojiCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var label: UILabel!
+}
